95359751deeff05bec74ba69729584a82b59eecf
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2021-07-29 19:18:48 -0300

    Basic send and recv of data

--- sockets/consumer/source/main.cpp
+++ sockets/consumer/source/main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include "Server.h"
+#include "BufferWriter.h"
+#include "BufferReader.h"
 
 using namespace std;
 
+#define BUFFER_LENGTH 100
+unsigned char buffer[BUFFER_LENGTH];
+BufferWriter writer;
+BufferReader reader;
+
+Server server(7000, 1, 100);
 void OnRecvFromClient(SOCKET client_handle, BUFFER buffer, int length);
 void OnClientConnected(SOCKET client_handle);
 void OnClientDisconnected(SOCKET client_handle);
 
 int main(void) {
-    Server server(7000, 1, 100);
+    writer.SetBuffer(buffer, BUFFER_LENGTH);
 
     server.SetCallbacks(&OnRecvFromClient, &OnClientConnected, &OnClientDisconnected);
 
@@ -18,12 +26,14 @@ int main(void) {
 
 void OnRecvFromClient(SOCKET client_handle, BUFFER buffer, int length)
 {
-    cout << "Data recevied from client: " << client_handle << endl; 
+    reader.SetBuffer((unsigned char*)buffer, length);
+    int32_t value = reader.ReadInt32();
+    cout << "Data recevied from client [" << client_handle << "]: " << value << endl;
 }
 
 void OnClientConnected(SOCKET client_handle)
 {
-    cout << "Client connected: " << client_handle << endl; 
+    cout << "Client connected: " << client_handle << endl;
 }
 
 void OnClientDisconnected(SOCKET client_handle)
--- sockets/producer/source/main.cpp
+++ sockets/producer/source/main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include "Client.h"
+#include "BufferWriter.h"
+#include "BufferReader.h"
 
 using namespace std;
 
+#define BUFFER_LENGTH 100
+unsigned char buffer[BUFFER_LENGTH];
+BufferWriter writer;
+BufferReader reader;
+
+Client client(7000, 100);
 void OnRecvFromServer(SOCKET client_handle, BUFFER buffer, int length);
 void OnConnected(SOCKET client_handle);
 void OnDisconnected(SOCKET client_handle);
 
 int main(void)
 {    
-    Client client(7000, 100);
     client.SetCallbacks(&OnRecvFromServer, &OnConnected, &OnDisconnected);
 
     if (!client.Connect("127.0.0.1"))
@@ -21,12 +28,15 @@ int main(void)
 
 void OnRecvFromServer(SOCKET client_handle, BUFFER buffer, int length)
 {
-    cout << "Data recevied from server" << endl; 
+    cout << "Data recevied from server" << endl;
 }
 
 void OnConnected(SOCKET client_handle)
 {
-    cout << "Connection to server stablished: " << client_handle << endl; 
+    cout << "Connection to server stablished: " << client_handle << endl;
+    writer.Reset();
+    writer.WriteInt32(100);
+    client.Send(writer); 
 }
 
 void OnDisconnected(SOCKET client_handle)
