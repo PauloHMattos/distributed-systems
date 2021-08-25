bf6ae87620bf311e44d56690eec9ec78377c4198
Gabriel Oliveira <gabriel.oliveira@cern.ch>
2021-08-24 21:32:53 -0300

    Updates producer-consumer code base.

--- tp2/producer-consumer/runner.py
+++ tp2/producer-consumer/runner.py
@@ -2,66 +2,53 @@ import matplotlib.pyplot as plt
 import numpy as np
 import subprocess
 
-buffer_sizes = ["1", "2", "4", "8", "16", "32"]
-threads_input = ["1", "2", "4", "8", "16"]
+buffer_sizes = ["1"]#, "2", "4", "8", "16", "32"]
+threads_input = [
+    ["1", "16"],
+    ["1", "8"],
+    ["1", "4"],
+    ["1", "2"],
+    ["1", "1"],
+    ["2", "1"],
+    ["4", "1"],
+    ["8", "1"],
+    ["16", "1"]
+]
 
 results = {}
+output_path = "data.csv"
 
-for buffer_size in buffer_sizes:
-    x = []
-    y = []
+with open(output_path, "w", newline='') as f:
+    f.write("Buffer size,# producer threads,# consumer threads,Time(ms)\n")
+    for buffer_size in buffer_sizes:
+        x = []
+        y = []
 
-    for ti in threads_input:
-        counter = 0
-        for i in range(10):
-            process = subprocess.Popen("dotnet run -- " + buffer_size + " 1 " + ti, stdout=subprocess.PIPE, shell=True)
-            (output, err) = process.communicate()
-            counter += int(output)
-        print ("With buffer size " + buffer_size + ", 1 producer and " + ti + " consumers, it took: " + str(counter/10))
+        for ti in threads_input:
+            counter = 0
+            for i in range(10):
+                process = subprocess.Popen("dotnet run -c Release -- " + ti[0] + " " + ti[1] + " " + buffer_size, stdout=subprocess.PIPE, shell=True)
+                (output, err) = process.communicate()
+                counter += int(output)
+            print ("With buffer size " + buffer_size + ", " + ti[0] + " producers and " + ti[1] + " consumers, it took: " + str(counter/10))
 
-        x.append(int(ti))
-        y.append(counter/10)
+            f.write("{}, {}, {}, {}\n".format(buffer_size, ti[0], ti[1], counter/10))
+            f.flush
+            x.append(int(ti[0])/int(ti[1]))
+            y.append(counter/10)
 
-    graph = {
-        "x": x, "y": y
-    }
-    results[buffer_size] = graph
+        graph = {
+            "x": x, "y": y
+        }
+        results[buffer_size] = graph
 
 for key, result in results.items():
     plt.plot(result["x"], result["y"], label = key)
 
 plt.title("Numero de Threads vs Tempo (ms)")
-plt.xlabel("Threads consumidoras")
+plt.xlabel("Np/Nc")
+plt.xscale("log", basex=2)
 plt.ylabel("Tempo (ms)")
 plt.legend()
-plt.savefig("1_producer_n_consumers.png")
+plt.savefig("producer_consumer.png")
 plt.clf()
-
-for buffer_size in buffer_sizes:
-    x = []
-    y = []
-
-    for ti in threads_input:
-        counter = 0
-        for i in range(10):
-            process = subprocess.Popen("dotnet run -- " + buffer_size + " " + ti + " 1", stdout=subprocess.PIPE, shell=True)
-            (output, err) = process.communicate()
-            counter += int(output)
-        print ("With buffer size " + buffer_size + ", " + ti + " producers and 1 consumer, it took: " + str(counter/10))
-
-        x.append(int(ti))
-        y.append(counter/10)
-
-    graph = {
-        "x": x, "y": y
-    }
-    results[buffer_size] = graph
-
-for key, result in results.items():
-    plt.plot(result["x"], result["y"], label = key)
-
-plt.title("Numero de Threads vs Tempo (ms)")
-plt.xlabel("Threads produtoras")
-plt.ylabel("Tempo (ms)")
-plt.legend()
-plt.savefig("n_producers_1_consumer.png")
--- tp2/producer-consumer/src/Program.cs
+++ tp2/producer-consumer/src/Program.cs
@@ -7,41 +7,51 @@ namespace producer_consumer
     {
         static int[] buffer;
         static int consumedNumbers = 0;
+        static int finishedThreads = 0;
         static Semaphore mutex;
         static Semaphore empty;
         static Semaphore full;
 
+        // Method used as an entry point for the program
         static void Main(string[] args)
         {
+            // Parsing the CLI arguments to store parameters
             int nProducers = Int32.Parse(args[0]);
             int nConsumers = Int32.Parse(args[1]);
             int bufferSize = Int32.Parse(args[2]);
 
+            // Initializing semaphores as described in the essay report
             empty = new Semaphore(bufferSize, bufferSize);
             full = new Semaphore(0, bufferSize);
             mutex = new Semaphore(1, 1);
             buffer = new int[bufferSize];
 
+            // Intializing a stopwatch to time the execution
             var watch = new System.Diagnostics.Stopwatch();
             watch.Start();
 
+            // Initializing an array of threads for the producers
             var producers = new Thread[nProducers];
             InitializeProducers(producers);
 
+            // Initializing an array of threads for the producers
             var consumers = new Thread[nConsumers];
             InitializeConsumers(consumers);
 
+            // Waits for every consumer thread to finish execution
             foreach(var t in consumers)
             {
                 t.Join();
             }
 
+            // Stops the stopwatch and prints the elapsed time
             watch.Stop();
             Console.WriteLine($"{watch.ElapsedMilliseconds}");
 
             Environment.Exit(0);
         }
 
+        // Method responsible for parsing the producers threads and starting them
         static void InitializeProducers(Thread[] producers)
         {
             for (var i = 0; i < producers.Length; i++)
@@ -51,31 +61,41 @@ namespace producer_consumer
             }
         }
 
+        // Method executed by each producer thread
         static void StartProducer()
         {
+            // Initializes random seeder
             var rng = new Random();
+
+            // Produces resources indefinitely
             while (true)
             {
+                // Waits for mutex and for a free slot in buffer
                 empty.WaitOne();
                 mutex.WaitOne();
 
+                // Iterate through buffer elements to find a free position
                 for (var i = 0; i < buffer.Length; i++)
                 {
+                    // Continues iteration if position is occupied
                     if (buffer[i] != 0)
                     {
                         continue;
                     }
 
+                    // Allocates first free position and breaks the loop
                     buffer[i] = (int)rng.Next(1, 10_000_001);
                     break;
                 }
 
+                // Releases mutex and signals a new resource available to be read
                 mutex.Release();
                 full.Release();
             }
         }
 
 
+        // Method responsible for parsing the consumers threads and starting them
         static void InitializeConsumers(Thread[] consumers)
         {
             for (var i = 0; i < consumers.Length; i++)
@@ -85,38 +105,54 @@ namespace producer_consumer
             }
         }
 
+        // Method executed by each consumer thread
         static void StartConsumer()
         {
+            int resource = -1;
+
+            // Execute threads until 10ˆ5 numbers were consumed colectively
             while (consumedNumbers < 100_000)
             {
-                int resource = -1;
-
+                // Waits for mutex and for an available resource in buffer
                 full.WaitOne();
                 mutex.WaitOne();
 
+                // Iterate through buffer elements to find an available resource
                 for (var i = 0; i < buffer.Length; i++)
                 {
+                    // Continues iteration if position is free
                     if (buffer[i] == 0)
                     {
                         continue;
                     }
 
+                    // Fetch first available resource and free its position
                     resource = buffer[i];
                     buffer[i] = 0;
                     break;
                 }
 
+                // Compute that a new number was consumed
                 consumedNumbers++;
 
+                // Releases mutex and signals a new free position in buffer
                 mutex.Release();
                 empty.Release();
 
+                // Prints whether read resource is prime or not
+                IsPrime(resource);
                 // var str = $"{resource} = {IsPrime(resource)}";
                 // Console.WriteLine(str);
                 // Check prime number
             }
+
+            // Compute a finished thread
+            mutex.WaitOne();
+            finishedThreads++;
+            mutex.Release();
         }
 
+        // Auxiliar method used to check whether a number is prime or not
         static bool IsPrime(int n)
         {
             if (n <= 1)
