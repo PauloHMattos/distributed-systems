be34ca551a3069dcbd526ac969b7869213da295f
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2021-09-11 18:24:09 -0300

    :sparkles: Basic client/server programs for case study

---
+++ tp3/Coordinator/src/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using TP3.Networking;
+
+namespace TP3.Coordinator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 128 is the max on TP3.pdf
+            var coordinator = new Coordinator(128);
+            coordinator.Start();
+
+            while(true)
+            {
+                coordinator.Update();
+            }
+        }
+    }
+
+
+    public class Coordinator
+    {
+        private readonly int _maxProcesses;
+        private readonly Peer _peer;
+
+        public Coordinator(int maxProcesses)
+        {
+            _maxProcesses = maxProcesses;
+            _peer = Peer.CreateServerPeer(Console.Out, 512, 512,  _maxProcesses, 5000);
+            _peer.AttachCallbacks(OnConnected, OnDisconnected, OnMessageReceived);
+        }
+
+        private void OnMessageReceived(ReadOnlySpan<byte> message, Connection connection)
+        {
+        }
+
+        private void OnDisconnected(Connection connection)
+        {
+        }
+
+        private void OnConnected(Connection connection)
+        {
+            Console.WriteLine("Connected!");
+        }
+
+        public void Start()
+        {
+            _peer.Listen(27000);
+        }
+
+        public void Update()
+        {
+            _peer.Update();
+        }
+    }
+}
---
+++ tp3/Process/src/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using TP3.Networking;
+
+namespace TP3.Process
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var process = new Process();
+            process.Start();
+
+            while(true)
+            {
+                process.Update();
+            }
+        }
+    }
+
+
+    public class Process
+    {
+        private readonly Peer _peer;
+
+        public Process()
+        {
+            _peer = Peer.CreateClientPeer(Console.Out, 512, 512, 5000);
+            _peer.AttachCallbacks(OnConnected, OnDisconnected, OnMessageReceived);
+        }
+
+        private void OnMessageReceived(ReadOnlySpan<byte> message, Connection connection)
+        {
+        }
+
+        private void OnDisconnected(Connection connection)
+        {
+        }
+
+        private void OnConnected(Connection connection)
+        {
+            Console.WriteLine("Connected!");
+        }
+
+        public void Start()
+        {
+            _peer.Connect(IPAddress.Parse("127.0.0.1"), 27000);
+        }
+
+        public void Update()
+        {
+            _peer.Update();
+        }
+    }
+}
