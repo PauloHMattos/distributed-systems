7f7367e36349bdf4a60709c8390e264a76cd0534
Gabriel Oliveira <gabriel.oliveira@cern.ch>
2021-10-03 19:20:58 -0300

    Add message handlers to coordinator.

--- tp3/Coordinator/src/Program.cs
+++ tp3/Coordinator/src/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Text;
 using System.Net;
 using TP3.Networking;
+using TP3.Common;
+using System.Collections.Concurrent;
 
 namespace TP3.Coordinator
 {
@@ -19,21 +23,86 @@ namespace TP3.Coordinator
         }
     }
 
+    internal class RequestMessageHandler : MessageHandler
+    {
+        public override MessageType MessageId => MessageType.Request;
+
+        private readonly ConcurrentQueue<Connection> _queue;
+
+        public RequestMessageHandler(ConcurrentQueue<Connection> queue)
+        {
+            _queue = queue;
+        }
+
+        public override void Handle(Connection connection, int? id)
+        {
+            _queue.Enqueue(connection);
+        }
+    }
+
+    internal class ReleaseMessageHandler : MessageHandler
+    {
+        public override MessageType MessageId => MessageType.Request;
+
+        private readonly ConcurrentQueue<Connection> _queue;
+
+        public ReleaseMessageHandler(ConcurrentQueue<Connection> queue)
+        {
+            _queue = queue;
+        }
+
+        public override void Handle(Connection connection, int? id)
+        {
+            if (!_queue.TryPeek(out var current))
+            {
+                Console.WriteLine("[ReleaseMessageHandler] Invalid Release. Empty Queue");
+                connection.Disconnect();
+                return;
+            }
+
+            if (current != connection)
+            {
+                Console.WriteLine($"[ReleaseMessageHandler] Invalid Release. {current.Index} != {connection.Index}");
+                connection.Disconnect();
+                return;
+            }
+
+            Debug.Assert(_queue.TryDequeue(out current));
+            Debug.Assert(current == connection);
+            
+            if (!_queue.TryPeek(out current))
+            {
+                return;
+            }
+
+            var grantMessage = Message.Build(MessageType.Grant);
+            current.SendMessage(grantMessage.Data);
+        }
+    }
 
     public class Coordinator
     {
         private readonly int _maxProcesses;
         private readonly Peer _peer;
+        private readonly MessageHandlerCollection _messageHandlerCollection;
+        private readonly ConcurrentQueue<Connection> _queue;
 
         public Coordinator(int maxProcesses)
         {
             _maxProcesses = maxProcesses;
             _peer = Peer.CreateServerPeer(Console.Out, 512, 512,  _maxProcesses, 5000);
             _peer.AttachCallbacks(OnConnected, OnDisconnected, OnMessageReceived);
+
+            _queue = new ConcurrentQueue<Connection>();
+            _messageHandlerCollection = new MessageHandlerCollection();
+
+            _messageHandlerCollection.AddHandler(new RequestMessageHandler(_queue));
+            _messageHandlerCollection.AddHandler(new ReleaseMessageHandler(_queue));
         }
 
         private void OnMessageReceived(ReadOnlySpan<byte> message, Connection connection)
         {
+            _messageHandlerCollection.Handle(connection, message);
         }
 
         private void OnDisconnected(Connection connection)
@@ -42,7 +111,11 @@ namespace TP3.Coordinator
 
         private void OnConnected(Connection connection)
         {
-            Console.WriteLine("Connected!");
+            Console.WriteLine($"Connected! {connection.EndPoint}");
+            
+            var message = Message.Build(MessageType.SetId);
+            Console.WriteLine(message.ToString());
+            connection.SendMessage(message.Data);
         }
 
         public void Start()
