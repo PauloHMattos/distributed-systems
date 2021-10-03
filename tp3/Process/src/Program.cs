7be4af00ae355a6b37ef04e1746f5b48c50a386c
Gabriel Oliveira <gabriel.oliveira@cern.ch>
2021-10-03 19:22:23 -0300

    Add message handlers to process.

--- tp3/Process/src/Program.cs
+++ tp3/Process/src/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Net;
+using System.IO;
+using System.Text;
 using TP3.Networking;
+using TP3.Common;
 
 namespace TP3.Process
 {
@@ -18,19 +21,93 @@ namespace TP3.Process
         }
     }
 
-
-    public class Process
+    internal class SetIdMessageHandler : MessageHandler
     {
+        public override MessageType MessageId => MessageType.SetId;
+
+        private readonly ProcessData _processData;
+
+        public SetIdMessageHandler(ProcessData processData)
+        {
+            _processData = processData;
+        }
+
+        public override void Handle(Connection connection, int? id)
+        {
+            if (!id.HasValue)
+            {
+                Console.WriteLine("[SetIdMessageHandler] Id not provided");
+                connection.Disconnect();
+                return;
+            }
+            _processData.Initialize(connection, id.Value);
+        }
+    }
+
+    internal class GrantMessageHandler : MessageHandler
+    {
+        public override MessageType MessageId => MessageType.Grant;
+
+        public override void Handle(Connection connection, int? id)
+        {
+            if (!id.HasValue)
+            {
+                Console.WriteLine("[SetIdMessageHandler] Id not provided");
+                connection.Disconnect();
+                return;
+            }
+
+            using var file = File.Open("resultado.txt", FileMode.Append);
+            var builder = new StringBuilder();
+            var timeString = DateTime.Now.ToString("hh:mm:ss");
+            builder.Append($"Id: {id}, time: {timeString}");
+
+            file.Write(Encoding.UTF8.GetBytes(builder.ToString()));
+
+            // TODO: Wait K seconds.
+        }
+    }
+
+    internal class ProcessData
+    {
+        public int? Index { get; private set; } = null;
+        public Connection? Connection { get; private set; }
+        public bool Initialized => Index.HasValue;
+
+        public void Initialize(Connection connection, int index)
+        {
+            if (Initialized)
+            {
+                throw new InvalidOperationException();
+            }
+            Connection = connection;
+            Index = index;
+            Console.WriteLine($"[ProcessData] index = {index}");
+        }
+    }
+
+    internal class Process
+    {
+        public ProcessData Data;
+
         private readonly Peer _peer;
+        private readonly MessageHandlerCollection _messageHandlerCollection;
 
         public Process()
         {
+            Data = new ProcessData();
+
             _peer = Peer.CreateClientPeer(Console.Out, 512, 512, 5000);
             _peer.AttachCallbacks(OnConnected, OnDisconnected, OnMessageReceived);
+
+            _messageHandlerCollection = new MessageHandlerCollection();
+
+            _messageHandlerCollection.AddHandler(new SetIdMessageHandler(Data));
         }
 
         private void OnMessageReceived(ReadOnlySpan<byte> message, Connection connection)
         {
+            _messageHandlerCollection.Handle(connection, message);
         }
 
         private void OnDisconnected(Connection connection)
@@ -39,7 +116,7 @@ namespace TP3.Process
 
         private void OnConnected(Connection connection)
         {
-            Console.WriteLine("Connected!");
+            Console.WriteLine($"OnConnected  {connection.EndPoint}");
         }
 
         public void Start()
