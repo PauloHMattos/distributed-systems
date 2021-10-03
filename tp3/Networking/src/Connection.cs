9252c3d6fc427bb8b47e57290b5f86efae1b053f
Gabriel Oliveira <gabriel.oliveira@cern.ch>
2021-10-03 19:21:27 -0300

    Add helper methods to connection.

--- tp3/Networking/src/Connection.cs
+++ tp3/Networking/src/Connection.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net;
 
 namespace TP3.Networking
@@ -9,11 +10,24 @@ namespace TP3.Networking
         public long LastReceivedTime;
         public long LastSendTime;
 
-        public Connection(IPEndPoint endPoint, byte index, long connectionTime)
+        private readonly Peer _peer;
+
+        public Connection(Peer peer, IPEndPoint endPoint, byte index, long connectionTime)
         {
+            _peer = peer;
             EndPoint = endPoint;
             Index = index;
             LastReceivedTime = connectionTime;
+        }
+
+        public void SendMessage(ReadOnlySpan<byte> message)
+        {
+            _peer.SendMessage(this, message);
+        }
+
+        public void Disconnect()
+        {
+            _peer.Disconnect(this);
         }
     }
 
