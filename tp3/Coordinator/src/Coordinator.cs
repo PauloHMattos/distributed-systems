2517cf8618b4b4c24d9c3765f82836d41ce46620
Gabriel Oliveira <gabriel.oliveira@cern.ch>
2021-10-06 19:44:33 -0300

    Adds thread for console interaction.

--- tp3/Coordinator/src/Coordinator.cs
+++ tp3/Coordinator/src/Coordinator.cs
@@ -3,6 +3,7 @@ using TP3.Networking;
 using TP3.Common;
 using System.Collections.Concurrent;
 using System.Threading;
+using System.IO;
 
 namespace TP3.Coordinator
 {
@@ -10,15 +11,19 @@ namespace TP3.Coordinator
     {
         private readonly int _maxProcesses;
         private readonly Peer _peer;
+        private readonly TextWriter _logger;
         private readonly MessageHandlerCollection _messageHandlerCollection;
         private readonly ConcurrentQueue<Connection> _queue;
         private readonly Thread _peerUpdateThread;
+        private readonly Thread _userInterfaceThread;
         public Connection? CurrentProcessWithLock { get; private set; }
 
         public Coordinator(int maxProcesses)
         {
+
             _maxProcesses = maxProcesses;
-            _peer = Peer.CreateServerPeer(Console.Out, 512, 512,  _maxProcesses, 5000);
+            _logger = File.CreateText("log.txt");
+            _peer = Peer.CreateServerPeer(_logger, 512, 512,  _maxProcesses, 5000);
             _peer.AttachCallbacks(OnConnected, OnDisconnected, OnMessageReceived);
 
             _queue = new ConcurrentQueue<Connection>();
@@ -28,6 +33,7 @@ namespace TP3.Coordinator
             _messageHandlerCollection.AddHandler(new ReleaseMessageHandler(this));
 
             _peerUpdateThread = new Thread(UpdatePeer);
+            _userInterfaceThread = new Thread(StartUserInterface);
         }
 
         private void OnMessageReceived(ReadOnlySpan<byte> message, Connection connection)
@@ -54,6 +60,7 @@ namespace TP3.Coordinator
         {
             _peer.Listen(27000);
             _peerUpdateThread.Start(_peer);
+            _userInterfaceThread.Start(_queue);
         }
 
         public void Update()
@@ -67,6 +74,31 @@ namespace TP3.Coordinator
                 ((Peer)peer!).Update();
                 // Sleep a bit to keep the CPU cool
                 Thread.Sleep(50);
+            }
+        }
+
+        public static void StartUserInterface(object? queue)
+        {
+            Console.Write("Waiting for one of the following commands:\n\n");
+            Console.Write("\t1: Prints the current requests queue.\n");
+            Console.Write("\t2: Prints how many times each process has been served.\n");
+            Console.Write("\t3: Finishes the execution.\n");
+            while (true) {
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        foreach (var connection in (ConcurrentQueue<Connection>)queue!) {
+                            Console.WriteLine(connection.Index);
+                        }
+                        break;
+                    case "2":
+                        // TODO
+                        break;
+                    case "3":
+                        Console.WriteLine("Finishing the execution...");
+                        Environment.Exit(0);
+                        break;
+                }
             }
         }
 
